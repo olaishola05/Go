ea90c29e87744eb5b495857a093d15e8c558e202
Oladipupo Ishola <olaishola@hotmail.co.uk>
2023-05-27 21:50:52 +0100

    refactor(app): test run
    Moved the mapping of servers to main

--- src/lectures/exercise/maps/maps.go
+++ src/lectures/exercise/maps/maps.go
@@ -23,18 +23,18 @@ func serverStatusStats(serverMap map[string]int) map[int]int {
 }
 
 func serverStatusInfo(title string, servers []string, serverMap map[string]int) {
-	totalServers := 0
-	var serverStats map[int]int
+	// totalServers := 0
+	// var serverStats map[int]int
 
-	for _, server := range servers {
-		totalServers++
-		serverMap[server] = Online
-		serverStats = serverStatusStats(serverMap)
-	}
+	// for _, server := range servers {
+	// 	totalServers++
+	// 	serverMap[server] = Online
+	// }
+	serverStats := serverStatusStats(serverMap)
 
 	fmt.Println()
 	fmt.Println("---", title, "---")
-	fmt.Println("Total number of server is:", totalServers)
+	fmt.Println("Total number of server is:", len(serverMap))
 	fmt.Println("Number of servers for Online", ":", serverStats[Online])
 	fmt.Println("Number of servers for Offline", ":", serverStats[Offline])
 	fmt.Println("Number of servers for Maintenance", ":", serverStats[Maintenance])
@@ -52,6 +52,12 @@ const (
 func main() {
 	servers := []string{"darkstar", "aiur", "omicron", "w359", "baseline"}
 	serverStatusMap := make(map[string]int)
+
+	for _, server := range servers {
+		// totalServers++
+		serverStatusMap[server] = Online
+		// serverStats = serverStatusStats(serverMap)
+	}
 
 	serverStatusInfo("Server Info", servers, serverStatusMap)
 

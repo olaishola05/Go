b0f0adb872151b42210753b6ba96ea6591109e97
Oladipupo Ishola <olaishola@hotmail.co.uk>
2023-05-28 18:34:19 +0100

    fix(app): Solve the server program
    Refactor code in smaller functions and create resuable func for repetitive tasks

--- src/lectures/exercise/maps/maps.go
+++ src/lectures/exercise/maps/maps.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+func serversToMap(servers []string, status int) map[string]int {
+	serverStatusMap := make(map[string]int)
+	for _, server := range servers {
+		serverStatusMap[server] = status
+	}
+	return serverStatusMap
+}
+
 func serverStatusStats(serverMap map[string]int) map[int]int {
 	serverStats := make(map[int]int)
 	for _, eachServerStatus := range serverMap {
@@ -22,24 +30,21 @@ func serverStatusStats(serverMap map[string]int) map[int]int {
 	return serverStats
 }
 
-func serverStatusInfo(title string, servers []string, serverMap map[string]int) {
-	// totalServers := 0
-	// var serverStats map[int]int
-
-	// for _, server := range servers {
-	// 	totalServers++
-	// 	serverMap[server] = Online
-	// }
+func serverStatusInfo(title string, serverMap map[string]int) {
 	serverStats := serverStatusStats(serverMap)
 
 	fmt.Println()
 	fmt.Println("---", title, "---")
-	fmt.Println("Total number of server is:", len(serverMap))
+	fmt.Println("Total number of servers is:", len(serverMap))
 	fmt.Println("Number of servers for Online", ":", serverStats[Online])
 	fmt.Println("Number of servers for Offline", ":", serverStats[Offline])
-	fmt.Println("Number of servers for Maintenance", ":", serverStats[Maintenance])
 	fmt.Println("Number of servers for retired", ":", serverStats[Retired])
+	fmt.Println("Number of servers for Maintenance", ":", serverStats[Maintenance])
+}
 
+func updateServer(serverStatusMap map[string]int, status int, server string) map[string]int {
+	serverStatusMap[server] = status
+	return serverStatusMap
 }
 
 const (
@@ -51,24 +56,15 @@ const (
 
 func main() {
 	servers := []string{"darkstar", "aiur", "omicron", "w359", "baseline"}
-	serverStatusMap := make(map[string]int)
 
-	for _, server := range servers {
-		// totalServers++
-		serverStatusMap[server] = Online
-		// serverStats = serverStatusStats(serverMap)
-	}
+	serverStatusMap := serversToMap(servers, Online)
+	serverStatusInfo("Server Info", serverStatusMap)
 
-	serverStatusInfo("Server Info", servers, serverStatusMap)
+	serverStatusMap = updateServer(serverStatusMap, Retired, "darkstar")
+	serverStatusMap = updateServer(serverStatusMap, Offline, "aiur")
+	serverStatusInfo("Server status updates", serverStatusMap)
 
-	serverStatusMap["darkstar"] = Retired
-	serverStatusMap["aiur"] = Offline
-	serverStatusInfo("Server status updates", servers, serverStatusMap)
-
-	for server, _ := range serverStatusMap {
-		serverStatusMap[server] = Maintenance
-	}
-
-	serverStatusInfo("New server update info", servers, serverStatusMap)
+	serverStatusMap = serversToMap(servers, Maintenance)
+	serverStatusInfo("All servers on maintainance", serverStatusMap)
 
 }

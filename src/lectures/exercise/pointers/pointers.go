f2ac33f233190d1ed7ff1b86b91c36d43aa68cb9
Oladipupo Ishola <olaishola@hotmail.co.uk>
2023-05-29 20:14:28 +0100

    refactor(app): Refactor the product slice
    Re write the slice and ignore the product when looping over slice array

--- src/lectures/exercise/pointers/pointers.go
+++ src/lectures/exercise/pointers/pointers.go
@@ -1,23 +1,49 @@
-//--Summary:
-//  Create a program that can activate and deactivate security tags
-//  on products.
-//
-//--Requirements:
-//* Create a structure to store items and their security tag state
-//  - Security tags have two states: active (true) and inactive (false)
-//* Create functions to activate and deactivate security tags using pointers
-//* Create a checkout() function which can deactivate all tags in a slice
-//* Perform the following:
-//  - Create at least 4 items, all with active security tags
-//  - Store them in a slice or array
-//  - Deactivate any one security tag in the array/slice
-//  - Call the checkout() function to deactivate all tags
-//  - Print out the array/slice after each change
-
 package main
 
 import "fmt"
 
-func main() {
+type SecurityTag bool
 
+type Product struct {
+	name string
+	tag  SecurityTag
+}
+
+func activate(tag *SecurityTag) {
+	*tag = Active
+}
+
+func deactivate(tag *SecurityTag) {
+	*tag = Inactive
+}
+
+func checkOut(slice []Product) {
+	for i, _ := range slice {
+		deactivate(&slice[i].tag)
+	}
+}
+
+const (
+	Active   = true
+	Inactive = false
+)
+
+func main() {
+	productSlice := []Product{
+		{"bags", Active},
+		{"shoe", Active},
+		{"shoe", Active},
+		{"shoe", Active},
+	}
+
+	fmt.Println("Initial Items", productSlice)
+
+	deactivate(&productSlice[0].tag)
+	fmt.Println("First item deactivated", productSlice)
+
+	checkOut(productSlice)
+	fmt.Println("All items deactivated", productSlice)
+
+	activate(&productSlice[3].tag)
+	fmt.Println("Item 3 activated", productSlice)
 }

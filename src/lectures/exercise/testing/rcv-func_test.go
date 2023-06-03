7231c1ce1bc4afac2f8ba28dc1679c24578b45e6
Oladipupo Ishola <olaishola@hotmail.co.uk>
2023-06-03 20:40:01 +0100

    test(app): Receiver func test
    demo test for queue and receiver func test

--- src/lectures/exercise/testing/rcv-func_test.go
+++ src/lectures/exercise/testing/rcv-func_test.go
@@ -10,3 +10,45 @@
 //
 //--Notes:
 //* Use `go test -v ./exercise/testing` to run these specific tests
+
+package main
+
+import "testing"
+
+func TestHealth(t *testing.T) {
+	player := Player{
+		name:      "kix",
+		health:    500,
+		maxHealth: 500,
+		energy:    2000,
+		maxEnergy: 2000,
+	}
+
+	player.addHealth(30)
+
+	if player.health > player.maxHealth {
+		t.Errorf("player maxHealth should not be more than: %v", player.maxHealth)
+	}
+
+	player.addEnergy(500)
+
+	if player.energy > player.maxEnergy {
+		t.Errorf("player energy can not be greater than it's maxEnergy: %v", player.maxEnergy)
+	}
+}
+
+func TestModifyPlayer(t *testing.T) {
+	player := Player{
+		name:      "kix",
+		health:    500,
+		maxHealth: 500,
+		energy:    2000,
+		maxEnergy: 2000,
+	}
+
+	player.modifyPlayerHealth(551)
+
+	if player.health < 0 {
+		t.Errorf("player health cannot go below %v", 0)
+	}
+}
